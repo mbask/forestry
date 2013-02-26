72ca5b7f3d49526d04b310f69b964a38d06ca9aa
Marco Bascietto <marco@bascietto.name>
2013-02-26 14:58:46 +0100

    Updated docs

---
+++ R/forestry.R
@@ -0,0 +1,26 @@
+###############################################################################
+#
+# forestry: An R packagefor common forestry related analyses. 
+# author: Marco Bascietto <marco.bascietto@ibaf.cnr.it>
+#
+# This is released under a GPL license.
+# Still in very unstable alpha version.
+#
+# Documentation was created using roxygen:
+###############################################################################
+
+#' @title Tools for common forestry related analyses. 
+#'
+#' 
+#' 
+#' @name forestry-package
+#' @aliases forestry
+#' @docType package
+#' @import plyr
+#' @author Marco Bascietto \email{marco.bascietto@@ibaf.cnr.it}
+#' @keywords package
+
+#' 
+#' 
+#' @references Source code is hosted at GitHub (\url{http://mbask.github.com/forestry/})
+NULL
