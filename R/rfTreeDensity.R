7d46b77defe8162931c7a4549ebb92db36b5c0c5
Marco Bascietto <marco@bascietto.name>
2013-02-26 16:10:14 +0100

    Updated docs

--- R/rfTreeDensity.R
+++ R/rfTreeDensity.R
@@ -1,6 +1,6 @@
 #' @title Tree density at forest level, based on a relascopic plot
 #'
-#' Tree density is estimated as \eqn{N=\phi \cdot \sum_{i=1}^n{\frac{1}{g_i}}} where \eqn{\phi} is the relascopic band used during sampling, \eqn{n} is the total count of trees found to be inside of the plot.
+#' Tree density is estimated as \eqn{N=\phi \cdot \sum_{i=1}^{n_\phi}{\frac{1}{g_i}}} where \eqn{\phi} is the relascopic band used during sampling, \eqn{n_\phi} is the total count of trees found to be inside of the plot measured with the \eqn{\phi} band.
 #' 
 #' The function guesses the unit of measure of tree dbhs (\emph{cm} or \emph{m}) if parameter \code{unit} is set to "\code{auto}":
 #' \itemize{
@@ -12,6 +12,7 @@
 #' @param relascopeBand the relascopic band used in the relascopic plot (1, 2, 4), default: 2
 #' @param unit a character value to indicate the unit of measure of \code{treeDbh}: \code{cm}, \code{m}, \code{auto} (default value)
 #' @return a single numeric value (estimate of count of trees per hectare), not rounded
+#' @seealso \code{\link{ttBasalArea}}
 #' @export
 #' @examples
 #' # dbh from an ideal even-aged plot
